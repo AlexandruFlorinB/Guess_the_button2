1c641e66faaa86ad7d864d6d1e618a86ffb21745
AlexandruFlorinB <101994570+AlexandruFlorinB@users.noreply.github.com>
2022-03-23 19:28:08 +0200

    Add files via upload

---
+++ guess_the_button2.js
@@ -0,0 +1,16 @@
+function generateButtons() {
+  let nrButtons = document.getElementById("buttons").value; 
+  let winNumber = Math.floor(Math.random() * nrButtons + 1);
+  for (let i = 1; i <= nrButtons; ++i) {
+    let btn = document.createElement("button");
+    btn.innerHTML = "Guess";
+    btn.onclick = function Display() {
+      if (i === winNumber) {
+        document.getElementById("buttons_list").innerHTML="You won";
+      } else {
+        document.getElementById("buttons_list").innerHTML="You lose";
+      }
+    }
+    document.getElementById("buttons_list").appendChild(btn);
+  }
+}
